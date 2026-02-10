3e8f01b749749ebcb9f4af41831a43b82b6ac812
nponchon <nponchon@student.42.fr>
2026-02-10 16:03:54 +0100

    chore: fix Makefile to use dependencies and hidden folder for objects

---
+++ Makefile
@@ -0,0 +1,81 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mrlouf                                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/02/10 13:57:00 by mrlouf            #+#    #+#              #
+#    Updated: 2026/02/10 13:57:00 by mrlouf           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME		= ft_nm
+
+SRC_DIR		= src
+INC_DIR		= inc
+LIBFT_DIR	= libft
+
+SRC		= main.c
+
+SRCDIR		= src
+SRCS		= $(addprefix $(SRCDIR)/, $(SRC))
+
+OBJDIR		= .obj
+OBJS		= $(addprefix $(OBJDIR)/, $(SRC:.c=.o))
+
+DEPDIR		= .dep
+DEPS		= $(addprefix $(DEPDIR)/, $(SRC:.c=.d))
+DEPDIRS		= $(DEPDIR)
+
+LIBFT		= $(LIBFT_DIR)/libft.a
+
+CC			= cc
+CFLAGS		= -Wall -Wextra -Werror -g
+INCLUDES	= -I$(INC_DIR) -I$(LIBFT_DIR)
+LDFLAGS		= -L$(LIBFT_DIR) -lft
+DFLAGS		= -MT $@ -MMD -MP
+
+GREEN		= \033[0;32m
+YELLOW		= \033[0;33m
+RESET		= \033[0m
+
+MAKE		= make
+
+all: $(NAME)
+
+$(NAME): $(LIBFT) $(OBJS) Makefile
+	@mkdir -p $(OBJDIR)
+	@echo "$(YELLOW)Linking $(NAME)...$(RESET)"
+	@$(CC) $(CFLAGS) $(OBJS) $(LDFLAGS) -o $(NAME)
+	@echo "$(GREEN)$(NAME) compiled successfully!$(RESET)"
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.c Makefile
+	@mkdir -p $(@D)
+	@echo "$(YELLOW)Compiling $<...$(RESET)"
+	@$(CC) $(CFLAGS) $(INCLUDES) -MT $@ -MMD -MP -c $< -o $@
+	@mkdir -p $(DEPDIR)
+	@mv $(patsubst %.o,%.d,$@) $(subst $(OBJDIR),$(DEPDIR),$(@D))/
+
+$(LIBFT):
+	@echo "$(YELLOW)Building libft...$(RESET)"
+	@$(MAKE) -C $(LIBFT_DIR)
+	@echo "$(GREEN)libft compiled successfully!$(RESET)"
+
+clean:
+	@echo "$(YELLOW)Cleaning object files...$(RESET)"
+	@$(MAKE) -C $(LIBFT_DIR) clean
+	@/bin/rm -f $(OBJS)
+	@echo "$(GREEN)Clean complete!$(RESET)"
+
+fclean: clean
+	@echo "$(YELLOW)Removing binaries...$(RESET)"
+	@$(MAKE) -C $(LIBFT_DIR) fclean
+	@/bin/rm -f $(NAME)
+	@echo "$(GREEN)Full clean complete!$(RESET)"
+
+re: fclean all
+
+-include $(DEPS)
+
+.PHONY: all clean fclean re
---
+++ src/main.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrlouf                                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 13:57:00 by mrlouf            #+#    #+#             */
+/*   Updated: 2026/02/10 13:57:00 by mrlouf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/nm.h"
+
+void	nm_error(const char *msg)
+{
+	ft_putstr_fd("ft_nm: ", 2);
+	ft_putendl_fd((char *)msg, 2);
+	exit(1);
+}
+
+void	nm_cleanup(t_nm *nm)
+{
+	if (nm->file_data)
+		munmap(nm->file_data, nm->file_size);
+}
+
+int	nm_parse_elf(t_nm *nm)
+{
+	nm->ehdr = (Elf64_Ehdr *)nm->file_data;
+
+	if (nm->ehdr->e_ident[EI_MAG0] != ELFMAG0 ||
+		nm->ehdr->e_ident[EI_MAG1] != ELFMAG1 ||
+		nm->ehdr->e_ident[EI_MAG2] != ELFMAG2 ||
+		nm->ehdr->e_ident[EI_MAG3] != ELFMAG3)
+	{
+		ft_putstr_fd("ft_nm: ", 2);
+		ft_putstr_fd(nm->filename, 2);
+		ft_putendl_fd(": file format not recognized", 2);
+		return (1);
+	}
+
+	ft_printf("ELF file detected: %s\n", nm->filename);
+	ft_printf("Entry point: 0x%lx\n", nm->ehdr->e_entry);
+
+	return (0);
+}
+
+void	nm_print_symbols(t_nm *nm)
+{
+	(void)nm;
+	ft_printf("Symbol table printing not yet implemented\n");
+}
+
+static int	nm_process_file(const char *filename)
+{
+	t_nm		nm;
+	int			fd;
+	struct stat	st;
+
+	ft_memset(&nm, 0, sizeof(t_nm));
+	nm.filename = (char *)filename;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_putstr_fd("ft_nm: ", 2);
+		ft_putstr_fd((char *)filename, 2);
+		ft_putendl_fd(": No such file", 2);
+		return (1);
+	}
+
+	if (fstat(fd, &st) < 0)
+	{
+		close(fd);
+		nm_error("fstat failed");
+	}
+
+	nm.file_size = st.st_size;
+	nm.file_data = mmap(NULL, nm.file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	close(fd);
+
+	if (nm.file_data == MAP_FAILED)
+		nm_error("mmap failed");
+
+	if (nm_parse_elf(&nm) == 0)
+		nm_print_symbols(&nm);
+
+	nm_cleanup(&nm);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	ret;
+
+	if (argc < 2)
+	{
+		ft_putendl_fd("Usage: ft_nm <file> [files...]", 2);
+		return (1);
+	}
+
+	ret = 0;
+	i = 1;
+	while (i < argc)
+	{
+		if (argc > 2)
+			ft_printf("\n%s:\n", argv[i]);
+		if (nm_process_file(argv[i]) != 0)
+			ret = 1;
+		i++;
+	}
+	
+	return (ret);
+}
